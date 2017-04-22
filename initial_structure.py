98eb8a2edf2e08ee0e0aa0767d335fe4c59f052f
mwweinberg <mwweinberg@users.noreply.github.com>
2017-04-22 14:42:20 -0400

    now with two storm

--- initial_structure.py
+++ initial_structure.py
@@ -3,14 +3,23 @@
 def relax():
     print("relaxing")
 
-def start_storm():
-    print("starting storm")
+def start_storm1():
+    print("starting storm1")
 
-def in_storm():
-    print("in the storm")
+def in_storm1():
+    print("in the storm1")
 
-def end_storm():
-    print("ending storm")
+def end_storm1():
+    print("ending storm1")
+
+def start_storm2():
+    print("starting storm2")
+
+def in_storm2():
+    print("in the storm2")
+
+def end_storm2():
+    print("ending storm2")
 
 #starts things off in the clear
 old_state = "clear"
@@ -24,16 +33,26 @@ while True:
     state = input("> ")
 
     #if we are already in a storm (this is triggered by the else below)
-    if old_state == "stormy":
+    if old_state == "stormy1":
 
         #"1" triggers the storm and keeps us in teh storm
         if state == "1":
-            in_storm()
-            old_state = "stormy"
+            in_storm1()
+            old_state = "stormy1"
 
         #if "1" isn't the input it is time to end the storm
         else:
-            end_storm()
+            end_storm1()
+            old_state = "clear"
+
+    elif old_state == "stormy2":
+
+        if state == "2":
+            in_storm2()
+            old_state = "stormy2"
+
+        else:
+            end_storm2()
             old_state = "clear"
 
     #if things are clear
@@ -42,9 +61,13 @@ while True:
         #if the input is to start a storm
         if state == "1":
             #run the start storm element
-            start_storm()
+            start_storm1()
             #and change the setting to "in a storm"
-            old_state = "stormy"
+            old_state = "stormy1"
+
+        elif state == "2":
+            start_storm2()
+            old_state = "stormy2"
 
         #if it is calm and the goal is to stay calm...
         else:
