340ce2e7dfda86a8a5dcc0cba1056e6909f0b2ab
Michael Weinberg <mwweinberg@users.noreply.github.com>
2018-02-27 06:08:30 -0500

    gets weatherID code

--- weatherapi.py
+++ weatherapi.py
@@ -1,16 +1,23 @@
 #python3
 
 #target API endpoint
-#http://api.openweathermap.org/data/2.5/weather?id=[API KEY]
+#http://api.openweathermap.org/data/2.5/weather?id=[locationID]&appid=[API KEY]
 
 #pyowm documentation:
 #https://github.com/csparpa/pyowm
+
+#weather code list:
+#https://openweathermap.org/weather-conditions
 
 #open weather map client wrapper
 import pyowm
 
 #imports the variables from the config.py file
 from config import *
+
+locationID = '4033936'
+
+print('http://api.openweathermap.org/data/2.5/weather?id='+locationID+'&appid='+API_KEY)
 
 owm = pyowm.OWM(API_KEY)
 
@@ -19,4 +26,10 @@ observation = owm.weather_at_id(4033936)
 w = observation.get_weather()
 print(w)
 
-print(w.get_wind())
+wind = w.get_wind()
+wind_speed = wind['speed']
+
+print(wind_speed)
+
+id = w.get_weather_code()
+print(id)
