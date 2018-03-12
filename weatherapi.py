d40bffe4706e884c6db75792e6dd12aa2e3a6cfa
Michael Weinberg <mwweinberg@users.noreply.github.com>
2018-03-11 20:40:04 -0400

    pulls data, finds specific time, caps dict

--- weatherapi.py
+++ weatherapi.py
@@ -1,43 +1,59 @@
 #python3
 
 #target API endpoint
-#http://api.openweathermap.org/data/2.5/weather?id=[locationID]&appid=[API KEY]
+#http://api.openweathermap.org/data/2.5/weather?id=[API KEY]
 
 #pyowm documentation:
 #https://github.com/csparpa/pyowm
 
-#weather code list:
-#https://openweathermap.org/weather-conditions
-
 #open weather map client wrapper
 import pyowm
+import time
 
 #imports the variables from the config.py file
 from config import *
 
-#this is just for testing to print out the URL it is hitting and can be removed
-locationID = '4033936'
-print('http://api.openweathermap.org/data/2.5/weather?id='+locationID+'&appid='+API_KEY)
+#defines the weather object
+owm = pyowm.OWM(API_KEY)
 
-def get_weather_code():
+condition_dict = {}
 
-    #sets up the weather object
-    owm = pyowm.OWM(API_KEY)
+while True:
+    #pulls the weather data
     observation = owm.weather_at_id(4033936)
+
     w = observation.get_weather()
+    condition = int(w.get_weather_code())
 
-    #gets the current weather code
-    weather_code = w.get_weather_code()
-    #for debugging
-    print(weather_code)
+    print(condition)
 
-    #looks for calm codes
-    if weather_code == 701 or 801 or 802 or 803 or 951 or 952 or 953 or 954 or 955:
-        print('calm state')
-        return 0
-    else:
-        print('storm_state')
-        return 1
+    #dictionary with current time (rounded to second) and condition
+    now_stamp = int(time.time())
+    condition_dict[now_stamp] = condition
+    print(condition_dict)
 
-get_weather_code()
-print(get_weather_code())
+    #looks for a key that is ~2 minutes ago
+    #this will be the function used to find the action match when
+    #it is integrated into the larger script
+    for i in condition_dict:
+        #if the key is within two minutes of now (+/- 20 seconds)
+        if (i + 100) <= now_stamp <= (i + 140):
+            #print this
+            print("Two minutes ago the condition was: " + str(i))
+
+    #temporary dict becuase you can't delete things in a dict duringa loop
+    #this will be the function used to cap the size of the dictionary
+    #once it is integrated into the larger script 
+    cleaning_dict = {}
+    #tries to clean up old keys
+    for i in condition_dict:
+        #if i is less than 240 seconds old
+        if now_stamp < (i + 240):
+            #add it to the temporary dictionary
+            cleaning_dict[i] = condition_dict[i]
+
+    #once the capped temp dict has been created swap it in for the old one
+    condition_dict = cleaning_dict
+
+
+    time.sleep(30)
