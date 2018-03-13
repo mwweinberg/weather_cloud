f54b564b887f177c659c32034f2f969b1dccd384
Michael Weinberg <mwweinberg@users.noreply.github.com>
2018-03-12 21:05:40 -0400

    initial commit

---
+++ weatherdb.py
@@ -0,0 +1,43 @@
+
+import sqlite3
+
+#open weather map client wrapper
+import pyowm
+import time
+
+#imports the variables from the config.py file
+from config import *
+
+#create the db
+db = sqlite3.connect('data/weatherdb')
+
+# Get a cursor object and create the table
+cursor = db.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users(time_stamp INTEGER,
+                       weather_code INTEGER)
+''')
+db.commit()
+
+#defines the weather object
+owm = pyowm.OWM(API_KEY)
+
+while True:
+
+    #pulls a time stamp
+    now_stamp = int(time.time())
+    #print("now_stamp = " + str(now_stamp))
+
+    #pulls the weather data
+    observation = owm.weather_at_id(4033936)
+    w = observation.get_weather()
+    condition = int(w.get_weather_code())
+    #print("condition = " + str(condition))
+
+    #writes the time stamp and weather data into the db
+    cursor.execute('''INSERT INTO users(time_stamp, weather_code)
+                      VALUES(?,?)''', (now_stamp,condition))
+    print("now_stamp = " + str(now_stamp) + " condition = " + str(condition) + " to db")
+    db.commit()
+
+    time.sleep(10)
